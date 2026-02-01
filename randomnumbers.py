3d393eb36b2ea28c03d8a845f8ceb9024be06c71
Kartik Attri <attrikartik494@gmail.com>
2026-02-01 16:34:07 +0530

    python -u d:\MachineLearning\tempCodeRunnerFile.py

--- randomnumbers.py
+++ randomnumbers.py
@@ -1,14 +1,34 @@
 import numpy as np
 
-print(np.random.rand(3))
+print("\n",np.random.rand(3))
 
 
-print(np.random.rand(3,5))
+print("\n",np.random.rand(3,5))
 
-print(np.random.randn(2,3))
+print("\n",np.random.randn(2,3))
 
 #from low to high 
-print(np.random.randint(2,100))
+print("\n",np.random.randint(2,100))
 
 #RANDOM IN THE FORM OF MULTIDIMENSION 
-print(np.random.randint(-4,8, size=(3,3)))
+print("\n",np.random.randint(-4,8, size=(3,3)))
+
+
+
+
+
+#important 
+rana = np.random.randint(1,10,8)
+print("\n Rana is :",rana)
+
+
+
+#max, min, argmax, argmin
+print(rana.max())
+print(rana.min())
+print(rana.argmax())  #-> returns the index of the largest in the array 
+print(rana.argmin())
+
+highest = rana.max()
+print("Highest:", highest)
+
