49d896545ef947132a15757c70c03508ef998562
Kartik Attri <attrikartik494@gmail.com>
2026-02-02 21:28:39 +0530

    fetching data from datarframe

---
+++ pandasdataframe2.py
@@ -0,0 +1,22 @@
+import pandas as pd
+df1=pd.DataFrame(
+    {
+        "Name":["Sharan","Jasleen","Jagroop","Seema","Meena","Teena","Deena","Taj","Raj","Pawan"],
+        "Rollno":[1,2,3,4,5,6,7,8,9,10],
+        "Gender":["Female","Female","Male","Female","Female","Female","Female","Female","Male","Male"],
+        "English":[90,56,67,87,67,89,89,89,78,90],
+     "Maths":[45,56,67,87,67,89,89,89,78,90],
+     "Science":[45,53,67,86,67,84,89,89,73,90],
+     "Hindi":[56,56,65,87,68,89,86,89,73,95],
+     "Punjabi":[66,56,64,87,69,89,87,89,78,93]
+
+
+    }
+
+)
+print(df1)
+
+#describe
+print('\n')
+print(df1.describe())
+
