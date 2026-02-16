ac7047701a9c91907be89612e36e1c121e70261d
Kartik Attri <attrikartik494@gmail.com>
2026-02-16 11:26:33 +0530

    multiple-lines

--- Matplotlib/matplotlib-marker-line-color.py
+++ Matplotlib/matplotlib-marker-line-color.py
@@ -3,6 +3,7 @@ import numpy as np
 
 yvalues=np.array([10,20,30,40,50,60])
 
-plt.plot(yvalues,'o:g') #takes x axis as 0 1 2 3 4 by default
+plt.plot(yvalues,'o:g',mec='red',mfc='red') #takes x axis as 0 1 2 3 4 by default
+#marker-edge-color and marker-face-color
 
 plt.show()
---
+++ Matplotlib/matplotlib-multiple-lines.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+values1=np.array([10,20,30,40,50,60])
+values2=np.array([90,34,54,64,75,75,85])
+plt.plot(values1)
+plt.plot(values2,color='black')
+
+plt.show()
