3adbe3c00f1dbd6beb888372b82a2db08f974c8b
Kartik Attri <attrikartik494@gmail.com>
2026-02-17 08:51:49 +0530

    matplotlib super title

---
+++ Matplotlib/matplotlib-multiple-plots3.py
@@ -0,0 +1,46 @@
+#You can draw as many plots you like on one figure, just descibe the number of rows, columns, and the index of the plot.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 1)
+plt.title("Title for 1st")
+plt.plot(x,y)
+
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 2)
+plt.title("Title for 2nd")
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 3)
+plt.title("Title for 3rd")
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 4)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 3, 5)
+plt.plot(x,y)
+
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 3, 6)
+plt.plot(x,y)
+
+plt.show()
