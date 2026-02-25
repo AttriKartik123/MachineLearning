c5ba0a4e660f560b17c3e5429f29bd57db90ef09
Kartik Attri <attrikartik494@gmail.com>
2026-02-25 11:52:43 +0530

    audio-file-upload

---
+++ streamlit/Audio-File-Camera.py
@@ -0,0 +1,68 @@
+
+import pandas as pd
+import streamlit as st
+
+audio_value = st.audio_input("Record a voice message")
+
+if audio_value:
+    st.audio(audio_value)
+
+
+
+#range
+#Higher sample rates can create higher-quality, larger audio files. This might require a nicer microphone to fully appreciate the difference
+
+
+import streamlit as st
+
+audio_value = st.audio_input("Record high quality audio", sample_rate=48000)
+
+if audio_value:
+    st.audio(audio_value)
+
+
+#data edit
+
+
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+edited_df = st.data_editor(df)
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+
+
+#Allowing users to add and delete rows
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+edited_df = st.data_editor(df, num_rows="dynamic")
+
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+
+
+
+#FILE UPLOADER
+
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
