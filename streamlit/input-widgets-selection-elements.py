d9472d48a95230f2b6e89452f17a9c0f70e30b95
Kartik Attri <attrikartik494@gmail.com>
2026-02-25 11:37:43 +0530

    selection-elements

---
+++ streamlit/input-widgets-selection-elements.py
@@ -0,0 +1,84 @@
+# multiselect
+import streamlit as st
+
+options = st.multiselect(
+    "What are your favorite colors?",
+    ["Green", "Yellow", "Red", "Blue"],
+    default=["Yellow", "Red"],
+)
+
+st.write("You selected:", options)
+
+
+#Let users to add new options
+
+
+options = st.multiselect(
+    "What are your favorite cat names?",
+    ["Jellybeans", "Fish Biscuit", "Madam President"],
+    max_selections=5,
+    accept_new_options=True,
+)
+
+st.write("You selected:", options)
+
+
+
+#PILLS ------
+
+
+options = ["North", "East", "South", "West"]
+selection = st.pills("Directions", options, selection_mode="multi")
+st.markdown(f"Your selected options: {selection}.")
+
+# Single-select pills with icons
+
+import streamlit as st
+
+option_map = {
+    0: ":material/add:",
+    1: ":material/zoom_in:",
+    2: ":material/zoom_out:",
+    3: ":material/zoom_out_map:",
+}
+selection = st.pills(
+    "Tool",
+    options=option_map.keys(),
+    format_func=lambda option: option_map[option],
+    selection_mode="single",
+)
+st.write(
+    "Your selected option: "
+    f"{None if selection is None else option_map[selection]}"
+)
+
+
+#radio btn 
+import streamlit as st
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
+
+if genre == ":rainbow[Comedy]":
+    st.write("You selected comedy.")
+else:
+    st.write("You didn't select comedy.")
+
+#To initialize an empty radio widget, use None as the index value:
+
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    index=None,
+)
+
+st.write("You selected:", genre)
+
