57b87193d51487e139695a6a2e3d3e7908c610c0
Kartik Attri <attrikartik494@gmail.com>
2026-02-26 20:02:48 +0530

    empty-done

---
+++ streamlit/layouts-empty.py
@@ -0,0 +1,65 @@
+#Examples
+#Inside a with st.empty(): block, each displayed element will replace the previous one.
+'''
+import streamlit as st
+import time
+
+with st.empty():
+    for seconds in range(10):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write(":material/check: 10 seconds over!")
+st.button("Rerun")
+'''
+#st.empty() creates a placeholder that you can update, replace, or clear later.
+
+
+
+
+#Example 2:
+'''
+import streamlit as st
+import time
+
+st.title("st.empty() Example")
+
+placeholder = st.empty()  # Create empty placeholder
+
+placeholder.write("Loading...")
+
+time.sleep(3)
+
+placeholder.write("Done!")
+'''
+
+
+import streamlit as st
+import time
+
+st.button("Start over")
+
+placeholder = st.empty()
+placeholder.markdown("Hello")
+time.sleep(1)
+
+placeholder.progress(0, "Wait for it...")
+time.sleep(1)
+placeholder.progress(50, "Wait for it...")
+time.sleep(1)
+placeholder.progress(100, "Wait for it...")
+time.sleep(1)
+
+with placeholder.container():
+    st.line_chart({"data": [1, 5, 2, 6]})
+    time.sleep(1)
+    st.markdown("3...")
+    time.sleep(1)
+    st.markdown("2...")
+    time.sleep(1)
+    st.markdown("1...")
+    time.sleep(1)
+
+placeholder.markdown("Poof!")
+time.sleep(1)
+
+placeholder.empty()
