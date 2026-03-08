a9e16ef42bb5aa5626dde1082a26b6568e76fdc1
Kartik Attri <attrikartik494@gmail.com>
2026-03-08 14:40:03 +0530

    map2

--- streamlit/Streamlit-map2.py
+++ streamlit/Streamlit-map2.py
@@ -6,3 +6,15 @@ file = st.file_uploader("Upload CSV")
 if file:
     data = pd.read_csv(file)
     st.map(data)
+
+
+
+#
+st.title("Location Map Example")
+
+data = pd.DataFrame({
+    "lat": [28.6139, 19.0760, 13.0827],
+    "lon": [77.2090, 72.8777, 80.2707]
+})
+
+st.map(data)
